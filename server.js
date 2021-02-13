955b92eded25402412cf98bf1651d43a9523cf3d
Ahmed Elnasseh <elnassehag@vcu.edu>
2021-02-13 10:38:48 -0500

    deploy version

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const cors=require('cors');
 const POKEDEX = require('./pokedex.json')
 
 const app = express()
+const morganSetting = process.env.NODE_ENV ==='production'? 'tiny': 'common'
 
-app.use(morgan('dev'))
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -50,11 +51,20 @@ app.get('/pokemon', function handleGetPokemon(req, res) {
         )
     }
     res.json(response)
-})
+});
 
+// 4 parameters in middleware, express knows to treat this as error handler
+app.use((error, req, res, next)=>{
+    let response
+    if(process.env.NODE_ENV === 'production'){
+        response = { error: {message: 'server error'}}
+    } else {
+        response = {error}
+    }
+    res.status(500).json(response)
+});
 
-
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`)
